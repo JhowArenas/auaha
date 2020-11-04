a0d64529ce109c9089f37d3005d8e3a171bf9282
JhowArenas <jonat.arenas@hotmail.com>
2020-11-04 01:24:59 -0300

    terminando

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,34 @@
+var screenWith = innerWidth;
+if (screenWith <= 1200) {
+    $("nav").append("<div class='shadow'></div>")
+}
+$(window).resize(function() {
+    screenWith = innerWidth;
+    $(".shadow").remove();
+    if (screenWith <= 1200) {
+        $("nav").append("<div class='shadow'></div>")
+    }
+
+    $("div.menus").removeClass("open");
+    $("div.shadow").removeClass("open");
+    $("html").removeClass("of-hidden");
+    $("body").removeClass("of-hidden");
+});
+
+$("#logo").click(function() {
+    if (screenWith < 1200) {
+        event.preventDefault();
+        $("div.menus").toggleClass("open");
+        $("div.shadow").toggleClass("open");
+        $("html").toggleClass("of-hidden");
+        $("body").toggleClass("of-hidden");
+    }
+})
+
+$(".shadow").click(function() {
+    $("#logo").click();
+})
+
 function nextItem(root) {
     var count = $("#" + root + " div").length;
     var firstElement = $("#" + root + " div").get(0);
